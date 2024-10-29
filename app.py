2553a4335e716ab76d77b287ddee5cf11c81f912
Abdelrahman Ahmed <seifmotaglly@gmail.com>
2024-10-29 14:23:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(protected_bp)
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
